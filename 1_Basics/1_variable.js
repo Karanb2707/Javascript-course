6b48939aeedc3be56967c7af80c9954ad54cf8aa
Karanb2707 <95834281+Karanb2707@users.noreply.github.com>
2025-03-17 23:15:41 +0530

    basic variable done

---
+++ 1_Basics/1_variable.js
@@ -0,0 +1,23 @@
+var accId = 123;
+let accName = 'Karan';
+const accEmail = 'karan@gmail.com';
+let accCity;
+
+//var
+// console.log(accId); // can reassign in var also can redeclare 
+// var accId = 234;
+// console.log(accId);
+
+//let
+// console.log(accName); // can reassign in let but cannot redeclare
+// accName = 'Raam';
+// console.log(accName);
+
+//const
+// accEmail = 'ram@gmail.com'; cannot reassign and redeclare
+// console.log(accEmail);
+
+//undefined
+//if variable declare and no value given to that variable it will assigned as undefined
+
+console.table([accId, accName, accEmail, accCity]);
