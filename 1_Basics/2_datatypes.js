9ecf375d720e97e5246d8044c3f1f7b18d7df80d
Karanb2707 <95834281+Karanb2707@users.noreply.github.com>
2025-03-19 21:19:13 +0530

    Data types done

---
+++ 1_Basics/2_datatypes.js
@@ -0,0 +1,29 @@
+"use strict" //Treat all js code as newer version
+
+//Data types eg
+
+let name = "Karan";
+let age = 21;
+let isLogin = false;
+
+//Primitive data types: 
+//Primitive datatypes represent single values and are immutable.
+
+// number => 2 to power 52
+// bigint => value grater than 2 to power 52
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => nothing assigned
+// symbol => unique // most used in react
+
+//Non-Primitive data types: 
+// Non-primitive types are objects and can store collections of data or more complex entities.
+
+// object => key-value pairs.
+// array => an ordered list of values
+// function => reusable blocks of code
+
+console.log(typeof "karan");
+console.log(typeof 1);
+console.log(typeof null);
