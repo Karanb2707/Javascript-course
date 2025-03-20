b3211f50844df75ef0f1c83b44494119e9f244c7
Karanb2707 <95834281+Karanb2707@users.noreply.github.com>
2025-03-20 21:56:48 +0530

    Conversion of data types is started

---
+++ 1_Basics/3_conversionOperations.js
@@ -0,0 +1,41 @@
+//Number
+
+// let score = "45";
+
+// console.log(typeof(score));
+
+// let numberInValue = Number(score);
+
+// console.log(typeof(numberInValue));
+
+
+// "33" => 33
+// "33abc" => NaN
+// null => 0
+
+
+//Boolean
+
+let $login = 1;
+
+console.log(typeof(1));
+
+let $booleanLogin = Boolean($login);
+
+console.log($booleanLogin);
+
+
+//1 => true
+//0 => false
+//"" => false
+//"karan" => true
+
+//String
+
+let $numberString = 27;
+
+console.log(typeof($numberString));
+
+let $stringCoversion = String($numberString);
+
+console.log(typeof($stringCoversion))
