70158ef0bdc7ad05aa829cf660301dddf5ea82cd
Karanb2707 <95834281+Karanb2707@users.noreply.github.com>
2025-03-25 22:12:51 +0530

    comparison operator

---
+++ 1_Basics/4_comparison.js
@@ -0,0 +1,27 @@
+// console.log(2 > 1);
+// console.log(2 < 1);
+// console.log(2 >= 1);
+// console.log(2 <= 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("2" < 1);
+
+//In JavaScript, when using comparison operators (>, <, >=, <=), null is converted to a number (0).
+
+//NULL
+// console.log(null > 0); false
+// console.log(null == 0); false
+// console.log(null >= 0); true
+
+//Undefined
+// console.log(undefined > 0);
+// console.log(undefined == 0);
+// console.log(undefined <= 0);
+//In every condition it gives false
+
+//=== Strict check
+//It checks value also the data type
+// console.log("2" == 2); true
+// console.log("2" === 2); false
