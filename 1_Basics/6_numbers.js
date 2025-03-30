1a511e169153b3e6996eba83458609aceb51c7f7
Karanb2707 <95834281+Karanb2707@users.noreply.github.com>
2025-03-30 13:22:35 +0530

    Numbers and Maths

---
+++ 1_Basics/6_numbers.js
@@ -0,0 +1,29 @@
+// const score = 400; //It can be number it can be string 
+// console.log(score);
+
+// const balance = new Number(400); //But it can only be number
+// console.log(balance);
+
+//Properties
+
+// console.log(balance.toString());
+// console.log(balance.toString().length); //we can use string properties
+// console.log(balance.toFixed(2)); 
+
+// const number = 101.2454;
+// console.log(number.toPrecision(4)); //It will give precise value and priority before the decimal
+
+// const number2 = 10000000;
+// console.log(number2.toLocaleString('en-IN')); //It will convert according indian number system
+
+//********************** Maths **********************/
+
+console.log(Math); //Its actual an object
+console.log(Math.abs(-5)); //Negative to possitive
+console.log(Math.round(4.3231)); //Round to nearest number
+
+console.log(Math.ceil(4.3)) //Convert to upper value
+console.log(Math.floor(4.8)) //Convert to lower value
+
+console.log(Math.min(1,2,4,9,5))
+console.log(Math.max(1,2,4,9,5))
