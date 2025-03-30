da1cf6b22a5796b4ad7290a325c799c92e07cb15
Karanb2707 <95834281+Karanb2707@users.noreply.github.com>
2025-03-30 12:29:25 +0530

    String

---
+++ 1_Basics/5_string.js
@@ -0,0 +1,21 @@
+// const name = "Karan"
+// const age = 21
+
+// console.log(name + ' age is ' + age); //Bad Practice
+
+//String Interpolation
+//String is object not array
+// console.log(`Hello, My Name is ${name} and age is ${age}`);x
+
+const cityName = new String('Mumbai');
+
+console.log(`${cityName} is city!`)
+
+// console.log(cityName[0]);
+// console.log(cityName.__proto__);
+
+// console.log(cityName.length)
+// console.log(cityName.toUpperCase());
+
+console.log(cityName.charAt(2)); // To find char at which position using index
+console.log(cityName.indexOf('b')) // To find index of char using that string
