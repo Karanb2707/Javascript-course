cf90c921d8f80d9a142babd3bfe1e86eed917a63
Karanb2707 <95834281+Karanb2707@users.noreply.github.com>
2025-03-30 12:51:12 +0530

    String done

--- 1_Basics/5_string.js
+++ 1_Basics/5_string.js
@@ -7,9 +7,9 @@
 //String is object not array
 // console.log(`Hello, My Name is ${name} and age is ${age}`);x
 
-const cityName = new String('Mumbai');
+// const cityName = new String('Mumbai');
 
-console.log(`${cityName} is city!`)
+// console.log(`${cityName} is city!`)
 
 // console.log(cityName[0]);
 // console.log(cityName.__proto__);
@@ -17,5 +17,26 @@ console.log(`${cityName} is city!`)
 // console.log(cityName.length)
 // console.log(cityName.toUpperCase());
 
-console.log(cityName.charAt(2)); // To find char at which position using index
-console.log(cityName.indexOf('b')) // To find index of char using that string
+// console.log(cityName.charAt(2)); // To find char at which position using index
+// console.log(cityName.indexOf('b')) // To find index of char using that string
+
+// const cityName2 = cityName.substring(0, 4); // in substring we can not give negative values
+// console.log(cityName2);
+
+// const cityName3 = cityName.slice(-2, 3); // in slice we can give negative values
+// console.log(cityName3);
+
+//Trim
+// const myname = "    Karan   "
+// console.log(myname);
+// console.log(myname.trim());
+
+//Replace
+// const url = "https://karan.com/karan%20bolake";
+
+// console.log(url);
+// console.log(url.replace('%20','-'));
+
+//Split
+const string1 = 'karan bolake'
+console.log(string1.split(' '));
