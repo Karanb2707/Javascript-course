a700c27420a5df7850d284f5b108cdefe12c01d9
Karanb2707 <95834281+Karanb2707@users.noreply.github.com>
2025-03-31 23:31:53 +0530

    Date JS

---
+++ 1_Basics/7_date_time.js
@@ -0,0 +1,25 @@
+//Dates
+
+// let date1 = new Date();
+
+// console.log(date1);
+// console.log(date1.toString());
+// console.log(date1.toDateString());
+// console.log(date1.toLocaleString());
+
+// console.log(typeof(date1)); //Object
+
+//Creating Date
+//In Js Month start from 0, 0 => Jan
+
+// let date2 = new Date(2025,1,20);
+// console.log(date2.toDateString());
+
+// let date3 = new Date(2025,6,27, 12,1);
+// console.log(date3.toLocaleString());
+
+let date4 = new Date("03-31-2025"); //Month-Date-Year
+console.log(date4);
+
+let myTimeStamp = new Date.now();
+console.log(myTimeStamp);
