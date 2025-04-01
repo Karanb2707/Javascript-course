c05cdc3bf12be73f16d9829194f0a483e5477010
Karanb2707 <95834281+Karanb2707@users.noreply.github.com>
2025-04-02 01:05:59 +0530

    Array start

---
+++ 2_Basics/1_array.js
@@ -0,0 +1,11 @@
+//Array
+
+let array1 = [1,2,3,6,7]
+
+console.log(array1);
+console.log(array1[2]);
+
+//Shallow copy: Shallow copy of an object is copy whose properties share the same references
+//If original array changed every copy will change
+
+//want to watch again
