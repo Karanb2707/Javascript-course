aa8b9045f71855edbe9a9fe1f9cd192569ab7bbc
Karan Bolake <95834281+Karanb2707@users.noreply.github.com>
2025-04-01 18:29:46 +0530

    Done Date type

--- 1_Basics/7_date_time.js
+++ 1_Basics/7_date_time.js
@@ -12,14 +12,33 @@
 //Creating Date
 //In Js Month start from 0, 0 => Jan
 
-// let date2 = new Date(2025,1,20);
+// let date2 = new Date(2025,1,20) //year,month,date;
 // console.log(date2.toDateString());
 
-// let date3 = new Date(2025,6,27, 12,1);
+// let date3 = new Date(2025,6,27, 12,1) //year,month,date,hour;
 // console.log(date3.toLocaleString());
 
-let date4 = new Date("03-31-2025"); //Month-Date-Year
-console.log(date4);
+// let date4 = new Date("03-31-2025"); //Month-Day-Year
+// console.log(date4);
 
-let myTimeStamp = new Date.now();
-console.log(myTimeStamp);
+// let myTimeStamp = Date.now();
+
+// console.log(myTimeStamp); //In this we get value in miliseconds from 1st jan 1970
+// console.log(date4.getTime()); //Same in this too
+
+//Mostly use to compare dates
+
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date();
+
+// console.log(newDate.getDate());
+// console.log(newDate.getMonth()); //In js month start from 0, 3 => April
+// console.log(newDate.getMinutes());
+
+let formatDate = newDate.toLocaleDateString('en-IND', {
+    dateStyle: 'long',
+    weekday: 'short'
+});
+
+console.log(formatDate);
