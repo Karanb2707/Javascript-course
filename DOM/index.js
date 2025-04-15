eae3aa8406d7f4eb0d3ad8bfd96b82b329af2b82
Karan Bolake <95834281+Karanb2707@users.noreply.github.com>
2025-04-15 18:35:57 +0530

    Add dom

---
+++ DOM/index.js
@@ -0,0 +1,17 @@
+//Window Object:
+//window object represents an open window in browser. its browser object not javascript
+//its global object with lots of properties & methods
+// alert('hello!');
+// console.log('hey');
+
+//DOM (Document Object Model):
+//when webpage load browser creates DOM of the page
+
+//1. Used for printing properties and methods
+// console.dir(window.document);
+
+//DOM Manipulation:
+
+//1. Selecting with id:
+let header = document.getElementById("header");
+console.log(header);
